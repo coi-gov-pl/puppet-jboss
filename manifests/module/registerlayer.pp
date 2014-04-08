676d3034cbbd58b7c532f7ce56fdbbfc28c53b60
Karol Lassak <karol.lassak@coi.gov.pl>
2014-04-08 13:20:20 +0200

    Improvement to setting off the JBoss module layouts

--- manifests/module/registerlayer.pp
+++ manifests/module/registerlayer.pp
@@ -3,11 +3,23 @@ define jboss::module::registerlayer (
 ) {
   include jboss
   
+  File {
+    mode   => '0640',
+    owner  => $jboss::jboss_user,
+    group  => $jboss::jboss_group,    
+  }
+
   if (!defined(Exec["jboss::module::layer::${layer}"])) {
     exec { "jboss::module::layer::${layer}":
       alias   => "layer_${layer}", # Deprecated
       command => "/bin/awk -F'=' 'BEGIN {ins = 0} /^layers=/ { ins = ins + 1; print \$1=${layer},\$2 } END {if(ins == 0) print \"layers=${layer},base\"}' > ${jboss::home}/modules/layers.conf",
       unless  => "/bin/egrep -e '^layers=.*${layer}.*' ${jboss::home}/modules/layers.conf",
+      require => Anchor["jboss::installed"],
+      notify  => Service['jboss'],
+    }
+    file { "jboss::module::layer::${layer}":
+      ensure  => directory,
+      path    => "${jboss::home}/modules/system/layers/${layer}",
       require => Anchor["jboss::installed"],
       notify  => Service['jboss'],
     }
