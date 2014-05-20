45add0023a5380ef20491b56e047074b7da7753c
Krzysztof Suszyński <krzysztof.suszynski@coi.gov.pl>
2014-05-20 16:03:12 +0200

    Calculating a proper initial config xml at runtime

---
+++ manifests/internal/runtime.pp
@@ -0,0 +1,25 @@
+/**
+ * JBoss internals - class that calculates and hold variables calculated at runtime
+ */
+class jboss::internal::runtime {
+  include jboss
+  
+  $runasdomain   = $jboss::runasdomain
+  $profile       = $jboss::profile
+  
+  $domainconfigfile = 'domain.xml'
+  
+  $standaloneconfigfile = $profile ? {
+    ''        => 'standalone.xml',
+    'ha'      => 'standalone-ha.xml',
+    'osgi'    => 'standalone-osgi.xml',
+    'full'    => 'standalone-full.xml',
+    'full-ha' => 'standalone-full-ha.xml',
+    default   => 'standalone-full.xml'
+  }
+  
+  $configfile = $runasdomain ? {
+    true    => $domainconfigfile,
+    default => $standaloneconfigfile,
+  }
+}
