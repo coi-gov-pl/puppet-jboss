22daa02235afe9d85f19b5cff43f020ddb525ed9
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2015-09-25 12:25:24 +0200

    Enh #1: Spec test for jboss_running fact

---
+++ spec/unit/facter/jboss_running_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'jboss_running', :type => :fact do
+  subject { Facter.value(:jboss_running) }
+  it { expect { subject }.not_to raise_error }
+  it { expect(subject).to match('^(?:true|false)$') }
+end
