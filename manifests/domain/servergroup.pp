3c51c264403cef3d4d60e7508fa3c2f97f891562
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2015-10-22 16:07:48 +0200

    Enh #6: Documentation for server and servergroup

--- manifests/domain/servergroup.pp
+++ manifests/domain/servergroup.pp
@@ -1,4 +1,40 @@
-# Creates JBoss domain server group
+# == Define: jboss::domain::servergroup
+#
+# This defined type simplifies creation and removal and updating JBoss domain server group that can enforce
+# same configuration (profile, deployments and JVM settings) across multiple servers on multiple host 
+# controllers. This is only possible in domain mode.
+#
+# === Parameters
+#
+# This type uses *JBoss module standard metaparameters*
+#
+# [*ensure*]
+#     Standard Puppet ensure parameter with values `present` and `absent`
+# [*permgensize*]
+#     This parameters configures JVM PermGen minimal size, By default it is equal to `32m`
+# [*maxpermgensize*]
+#     This parameters configures JVM PermGen maximal size, By default it is equal to `256m`
+# [*heapsize*]
+#     This parameters configures JVM Heap minimal size, By default it is equal to `256m`
+# [*maxheapsize*]
+#     This parameters configures JVM Heap maximal size, By default it is equal to `1303m`
+# [*profile*]
+#     This parameter configure profile to be active on all servers within this group. By default 
+#     it is equal to value of `$::jboss::profile`.
+# [*socket_binding_group*]
+#     This parameter indicates that server instances within this group should use value of it as socket 
+#     binding group, By default it is set to `full-sockets`.
+# [*socket_binding_port_offset*]
+#     This parameter indicates offset on JBoss ports, defined by socket binding group. It will shift ports up
+#     or down by amount described. The format is simply `n` or `-n`, for example: `120` will shift all ports by
+#     120 up making standard http port being now `8080 + 120 = 8200`. By default it is equal to `0`.
+# [*system_properties*]
+#     This parameter can be used to set system properties that will be passed to server instance java 
+#     process as `-D` parameters. It's empty by default.
+# [*jvmopts*]
+#     This property can be set to configure additional JVM options to be passed to server instances in
+#     addition to standard memory configuration.
+#
 define jboss::domain::servergroup (
   $ensure                     = 'present',
   $permgensize                = $::jboss::internal::params::memorydefaults::permgensize,
