91f2c3e8f0004bba9399b11368d62eb7ccc19d5d
André Frimberger <andre@frimberger.de>
2015-11-19 21:46:35 +0100

    Fix puppet-lint warnings

--- manifests/internal/configure/conf_entry.pp
+++ manifests/internal/configure/conf_entry.pp
@@ -1,15 +1,15 @@
 # Internal class - one line in .conf file
 define jboss::internal::configure::conf_entry(
+  $value,
   $ensure   = undef,
   $variable = $name,
-  $value
 ) {
   include jboss::internal::runtime
 
   if $ensure == undef {
     $auto_ensure = $value ? {
       undef   => absent,
-      ""      => absent,
+      ''      => absent,
       default => present,
     }
   } else {
@@ -20,10 +20,10 @@ define jboss::internal::configure::conf_entry(
   $marker      = "### PUPPET_${variable}_MARKER ###"
 
   file_line { "${configfile}: ${variable}=${value}":
-    ensure  => $auto_ensure,
-    path    => $configfile,
-    line    => "${variable}=\"${value}\" ${marker}",
-    match   => "\\s*${variable}=.*${marker}",
+    ensure => $auto_ensure,
+    path   => $configfile,
+    line   => "${variable}=\"${value}\" ${marker}",
+    match  => "\\s*${variable}=.*${marker}",
   }
 
 }
