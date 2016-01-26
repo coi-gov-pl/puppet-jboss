7234c7c5f1945f6e6264a7709c115b130ca99084
Morga Cezary <cezary.morga@coi.gov.pl>
2016-01-26 14:47:51 +0100

    bugfic/14 Set default to Wildfly version 9.0.2.Final

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -15,8 +15,8 @@ describe 'jboss', :type => :class do
     it do
       is_expected.to contain_class('jboss').with({
         :product      => 'wildfly',
-        :version      => '8.2.0.Final',
-        :download_url => 'http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.zip'
+        :version      => '9.0.2.Final',
+        :download_url => 'http://download.jboss.org/wildfly/9.0.2.Final/wildfly-9.0.2.Final.zip'
         })
     end
     it { is_expected.to contain_anchor 'jboss::begin' }
