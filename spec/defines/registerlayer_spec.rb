cb4520dd765c274fbdd843789ad1f501e292c04c
Grzegorz Jędryka <grzegorz.jedryka@coi.gov.pl>
2016-02-23 15:22:45 +0100

    Added verification of Exec command and unless parameters.

--- spec/defines/registerlayer_spec.rb
+++ spec/defines/registerlayer_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper_puppet'
 describe 'jboss::internal::module::registerlayer', :type => :define do
   let(:title) { 'testlayer' }
   let(:params) { { :layer => title, } }
-  let(:pre_condition)  { "class { jboss: product => '#{product}', version => '#{version}', jboss_user => '#{user}', jboss_group => '#{group}' }" }
+  let(:pre_condition)  { "class { jboss: product => '#{product}', version => '#{version}', jboss_user => '#{user}', jboss_group => '#{group}', install_dir => '#{dir}' }" }
   let(:product) {'wildfly'}
   let(:version) {'9.0.2.Final'}
+  let(:dir) {'/jboss'}
   let(:user) {'test-user'}
   let(:group) {'test-group'}
   let(:facts) do
     {
-    :osfamily             => 'RedHat',
-    :operatingsystem      => 'OracleLinux',
-    :concat_basedir       => '/tmp/'
+    :osfamily        => 'RedHat',
+    :operatingsystem => 'OracleLinux',
+    :concat_basedir  => '/tmp/'
     }
   end
 
@@ -20,10 +21,12 @@ describe 'jboss::internal::module::registerlayer', :type => :define do
   it { is_expected.to contain_class 'jboss' }
   it { is_expected.to contain_jboss__internal__module__registerlayer("#{title}") }
   it { is_expected.to contain_exec("jboss::module::layer::#{title}").with({
-    :user => 'test-user'
+    :command => "/bin/awk -F'=' 'BEGIN {ins = 0} /^layers=/ { ins = ins + 1; print \$1=testlayer,\$2 } END {if(ins == 0) print \"layers=testlayer,base\"}' > #{dir}/#{product}-#{version}/modules/layers.conf",
+    :unless  => "/bin/egrep -e '^layers=.*testlayer.*' #{dir}/#{product}-#{version}/modules/layers.conf",
+    :user    => 'test-user'
     }) }
   it {
-    is_expected.to contain_file("/usr/lib/#{product}-#{version}/modules/system/layers/#{title}").with({
+    is_expected.to contain_file("#{dir}/#{product}-#{version}/modules/system/layers/#{title}").with({
     :ensure => 'directory',
     :owner  => 'test-user',
     :group  => 'test-group',
