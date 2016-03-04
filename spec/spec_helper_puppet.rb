19c5e36a46ac8599d8747ed1f43b3f03e00622bb
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2016-03-04 12:18:20 +0100

    Adding shared facts

--- spec/spec_helper_puppet.rb
+++ spec/spec_helper_puppet.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 module Testing
   module JBoss end
 end
+require 'testing/jboss/shared_facts'
 require 'testing/jboss/shared_examples'
 
 at_exit { RSpec::Puppet::Coverage.report! }
