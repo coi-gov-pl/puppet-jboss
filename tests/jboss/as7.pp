f98adf69903b3b3ca7dda6b58bae6fc48391dafa
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2016-03-07 22:52:07 +0100

    Partially working JBoss AS 7 acceptance test with errors described in #65

--- tests/jboss/as7.pp
+++ tests/jboss/as7.pp
@@ -1,6 +1,7 @@
 class { 'jboss':
   product       => 'jboss-as',
   version       => '7.1.1.Final',
+  # FIXME: Download url shouldn't be nessesary - GH Issue #65
   download_url  => 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip',
   enableconsole => true,
 }
