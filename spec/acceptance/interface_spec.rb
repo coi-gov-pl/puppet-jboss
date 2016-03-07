b27bfa19e94751f7aacf8a255add04abbd875c3a
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2016-03-07 22:05:28 +0100

    Working interface acceptance test

---
+++ spec/acceptance/interface_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper_acceptance'
+
+describe 'interface.pp smoke test', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+  let(:baseserver) { Testing::Acceptance::SmokeTestReader.smoke_pp :init }
+  let(:pp) { Testing::Acceptance::SmokeTestReader.smoke_pp :interface }
+
+  it 'should install base server with no errors' do
+    apply_manifest(baseserver, :catch_failures => true)
+  end
+  it 'should add resource adapter with no errors' do
+    apply_manifest(pp, :catch_failures => true)
+  end
+  it 'should work idempotently' do
+    apply_manifest(pp, :catch_changes  => true)
+  end
+  describe service('wildfly') do
+    it { is_expected.to be_running }
+  end
+  after(:all) do
+    extend Testing::Acceptance::Cleaner
+    remove_jboss_installation('wildfly')
+  end
+end
