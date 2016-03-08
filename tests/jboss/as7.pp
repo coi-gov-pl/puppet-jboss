b0ccadd62cfb97865e06744b54c0e3ca31898b71
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2016-03-08 20:21:55 +0100

    Additional tests for WildFly 8 and 9

--- tests/jboss/as7.pp
+++ tests/jboss/as7.pp
@@ -1,7 +1,6 @@
 class { 'jboss':
-  product       => 'jboss-as',
-  version       => '7.1.1.Final',
+  product      => 'jboss-as',
+  version      => '7.1.1.Final',
   # FIXME: Download url shouldn't be nessesary - GH Issue #65
-  download_url  => 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip',
-  enableconsole => true,
+  download_url => 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip',
 }
