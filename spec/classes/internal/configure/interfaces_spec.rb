c9bc890606bc957fb38becd14cb3d1568a63296f
Kozakowski Karol <karol.kozakowski@coi.gov.pl>
2016-03-08 10:22:16 +0100

    better tests for augas, configuration, inteface and etc_initd_funtions

--- spec/classes/internal/configure/interfaces_spec.rb
+++ spec/classes/internal/configure/interfaces_spec.rb
@@ -1,23 +1,26 @@
 require 'spec_helper_puppet'
 
 describe 'jboss::internal::configure::interfaces', :type => :define do
-  shared_examples 'completly working define' do
+  shared_examples 'contains self' do
     it { is_expected.to contain_class 'jboss::internal::configure::interfaces' }
+    it { is_expected.to contain_class('jboss') }
+    it { is_expected.to contain_class('jboss::params') }
+    it { is_expected.to contain_class('jboss::internal::runtime::dc') }
   end
 
   context 'On RedHat os family' do
     extend Testing::JBoss::SharedExamples
     let(:title) { 'test-conf-interfaces' }
     let(:facts) { Testing::JBoss::SharedFacts.oraclelinux_facts }
-    it_behaves_like 'completly working define'
-    it_behaves_like_full_working_jboss_installation
+
+    it_behaves_like 'contains self'
   end
 
   context 'On Debian os family' do
     extend Testing::JBoss::SharedExamples
     let(:title) { 'test-conf-interfaces' }
     let(:facts) { Testing::JBoss::SharedFacts.ubuntu_facts }
-    it_behaves_like 'completly working define'
-    it_behaves_like_full_working_jboss_installation
+
+    it_behaves_like 'contains self'
   end
 end
--- spec/classes/internal/quirks/etc_initd_functions_spec.rb
+++ spec/classes/internal/quirks/etc_initd_functions_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper_puppet'
 
 describe 'jboss::internal::quirks::etc_initd_functions', :type => :define do
-  shared_examples 'completly working define' do
+  shared_examples 'contains class structure' do
     it { is_expected.to contain_class 'jboss::internal::quirks::etc_initd_functions' }
+    it { is_expected.to contain_class('jboss') }
+    it { is_expected.to contain_class('jboss::internal::service') }
+    it { is_expected.to contain_class('jboss::internal::compatibility') }
   end
 
   context 'On RedHat os family' do
     extend Testing::JBoss::SharedExamples
     let(:title) { 'test-etc_initd_functions' }
     let(:facts) { Testing::JBoss::SharedFacts.oraclelinux_facts }
-    it_behaves_like 'completly working define'
-    it_behaves_like_full_working_jboss_installation
+    it_behaves_like 'contains class structure'
 
   end
 
@@ -18,8 +20,7 @@ describe 'jboss::internal::quirks::etc_initd_functions', :type => :define do
     extend Testing::JBoss::SharedExamples
     let(:title) { 'test-etc_initd_functions' }
     let(:facts) { Testing::JBoss::SharedFacts.ubuntu_facts }
-    it_behaves_like 'completly working define'
-    it_behaves_like_full_working_jboss_installation
+    it_behaves_like 'contains class structure'
 
   end
 end
