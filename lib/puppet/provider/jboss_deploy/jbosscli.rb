8737f7095e8f7cae317c493843b99d937cedeee0
Kozakowski Karol <karol.kozakowski@coi.gov.pl>
2016-03-15 09:50:10 +0100

    style fix for bad indent in deploy

--- lib/puppet/provider/jboss_deploy/jbosscli.rb
+++ lib/puppet/provider/jboss_deploy/jbosscli.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../../../puppet_x/coi/jboss'))
 
 Puppet::Type.type(:jboss_deploy).provide(:jbosscli,
-:parent => Puppet_X::Coi::Jboss::Provider::AbstractJbossCli) do
+  :parent => Puppet_X::Coi::Jboss::Provider::AbstractJbossCli) do
+
   desc 'JBoss CLI deploy provider'
   include Puppet_X::Coi::Jboss::Provider::Deploy
 end
