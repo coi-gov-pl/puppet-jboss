38ff219524586c81e7783fd0e108e5e4e508c32d
Kozakowski Karol <karol.kozakowski@coi.gov.pl>
2016-03-23 11:54:19 +0100

    correct handling of jboss as 7 installation without download url

--- manifests/internal/runtime.pp
+++ manifests/internal/runtime.pp
@@ -5,9 +5,10 @@ class jboss::internal::runtime {
   # Base URL for downloading Jboss Application Server installation package
   $download_urlbase = hiera('jboss::params::download_urlbase', 'http://download.jboss.org')
 
-  if $jboss::product == 'jboss-as' {
+  f $jboss::product == 'jboss-as' {
     $as_version = jboss_short_version($jboss::version)
-    $download_url = hiera('jboss::params::download_url', "${download_urlbase}/${jboss::product}/${as_version}/${jboss::version}/${jboss::product}-${jboss::version}.zip")
+    $trimmed_product_name = regsubst($jboss::product, '-', '')
+    $download_url = hiera('jboss::params::download_url', "${download_urlbase}/${trimmed_product_name}/${as_version}/${jboss::product}-${jboss::version}/${jboss::product}-${jboss::version}.zip")
   } else {
     # Full URL for downloading JBoss Application Server installation package
     $download_url     = hiera('jboss::params::download_url', "${download_urlbase}/${jboss::product}/${jboss::version}/${jboss::product}-${jboss::version}.zip")
