7104ef5fbfee2d5e431ab60580b5dc382f50d813
Kozakowski Karol <karol.kozakowski@coi.gov.pl>
2016-03-23 12:04:11 +0100

    typo

--- manifests/internal/runtime.pp
+++ manifests/internal/runtime.pp
@@ -5,7 +5,7 @@ class jboss::internal::runtime {
   # Base URL for downloading Jboss Application Server installation package
   $download_urlbase = hiera('jboss::params::download_urlbase', 'http://download.jboss.org')
 
-  f $jboss::product == 'jboss-as' {
+  if $jboss::product == 'jboss-as' {
     $as_version = jboss_short_version($jboss::version)
     $trimmed_product_name = regsubst($jboss::product, '-', '')
     $download_url = hiera('jboss::params::download_url', "${download_urlbase}/${trimmed_product_name}/${as_version}/${jboss::product}-${jboss::version}/${jboss::product}-${jboss::version}.zip")
