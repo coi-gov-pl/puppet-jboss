3ceba173be7d80e17b7543006d7087a041ca7d3f
Kozakowski Karol <karol.kozakowski@coi.gov.pl>
2016-03-23 14:33:06 +0100

    internal variables

--- manifests/internal/defaults.pp
+++ manifests/internal/defaults.pp
@@ -3,14 +3,14 @@
 class jboss::internal::defaults {
 
   include jboss
-  
+
   # Base URL for downloading Jboss Application Server installation package
   $download_urlbase = hiera('jboss::params::download_urlbase', 'http://download.jboss.org')
 
   if $jboss::product == 'jboss-as' {
-    $as_version = jboss_short_version($jboss::version)
-    $trimmed_product_name = regsubst($jboss::product, '-', '')
-    $download_url = hiera('jboss::params::download_url', "${download_urlbase}/${trimmed_product_name}/${as_version}/${jboss::product}-${jboss::version}/${jboss::product}-${jboss::version}.zip")
+    $__as_version = jboss_short_version($jboss::version)
+    $__trimmed_product_name = regsubst($jboss::product, '-', '')
+    $__download_url = hiera('jboss::params::download_url', "${download_urlbase}/${__trimmed_product_name}/${__as_version}/${jboss::product}-${jboss::version}/${jboss::product}-${jboss::version}.zip")
   } else {
     # Full URL for downloading JBoss Application Server installation package
     $download_url     = hiera('jboss::params::download_url', "${download_urlbase}/${jboss::product}/${jboss::version}/${jboss::product}-${jboss::version}.zip")
