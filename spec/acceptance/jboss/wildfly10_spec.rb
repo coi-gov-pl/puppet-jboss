ca7d1db961fdcb2396b0f2264ccb6142f0282c69
Morga Cezary <cezary.morga@coi.gov.pl>
2017-01-23 12:19:14 +0100

    Acceptance tests for Wildfly 10

--- spec/acceptance/jboss/wildfly10_spec.rb
+++ spec/acceptance/jboss/wildfly10_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper_acceptance'
 
 describe 'jboss::wildfly10 smoke test', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
-  let(:pp) { Testing::Acceptance::SmokeTestReader.smoke_pp :'jboss::wildfly10' }
+  shared_examples 'a properly working class on a platform with supported Java version' do
+    let(:pp) { Testing::Acceptance::SmokeTestReader.smoke_pp :'jboss::wildfly10' }
 
-  it 'should add install WildFly 10 with no errors' do
-    apply_manifest(pp, :expect_changes => true, :trace => true)
+    it 'should install WildFly 10 with no errors' do
+      apply_manifest(pp, :expect_changes => true, :trace => true)
+    end
+    it 'should work idempotently' do
+      apply_manifest(pp, :catch_changes  => true, :trace => true)
+    end
+    describe service('wildfly') do
+      it { is_expected.to be_running }
+    end
+    after(:all) do
+      extend Testing::Acceptance::Cleaner
+      remove_jboss_installation('wildfly')
+    end
   end
-  it 'should work idempotently' do
-    apply_manifest(pp, :catch_changes  => true, :trace => true)
-  end
-  describe service('wildfly') do
-    it { is_expected.to be_running }
-  end
-  after(:all) do
-    extend Testing::Acceptance::Cleaner
-    remove_jboss_installation('wildfly')
+
+  if (fact('osfamily') == 'RedHat' && fact('operatingsystemmajrelease') >= '7')
+    it_behaves_like 'a properly working class on a platform with supported Java version'
+  elsif (fact('operatingsystem') == 'Debian' && fact('operatingsystemmajrelease') >= '8' )
+    it_behaves_like 'a properly working class on a platform with supported Java version'
+  elsif (fact('operatingsystem') == 'Ubuntu' && fact('operatingsystemmajrelease') >= '15.10' )
+    it_behaves_like 'a properly working class on a platform with supported Java version'
   end
 end
