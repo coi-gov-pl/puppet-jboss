7990abe47d256c4d3d2eb171857d0b808d4c13d3
Kuczyński Konrad <Konrad.Kuczynski@coi.gov.pl>
2017-09-06 14:32:32 +0200

    enh-96 without rubocop tests

--- manifests/domain/servergroup.pp
+++ manifests/domain/servergroup.pp
@@ -1,7 +1,7 @@
 # == Define: jboss::domain::servergroup
 #
 # This defined type simplifies creation and removal and updating JBoss domain server group that can enforce
-# same configuration (profile, deployments and JVM settings) across multiple servers on multiple host 
+# same configuration (profile, deployments and JVM settings) across multiple servers on multiple host
 # controllers. This is only possible in domain mode.
 #
 # === Parameters
@@ -19,17 +19,17 @@
 # [*maxheapsize*]
 #     This parameters configures JVM Heap maximal size, By default it is equal to `1303m`
 # [*profile*]
-#     This parameter configure profile to be active on all servers within this group. By default 
+#     This parameter configure profile to be active on all servers within this group. By default
 #     it is equal to value of `$jboss::profile`.
 # [*socket_binding_group*]
-#     This parameter indicates that server instances within this group should use value of it as socket 
+#     This parameter indicates that server instances within this group should use value of it as socket
 #     binding group, By default it is set to `full-sockets`.
 # [*socket_binding_port_offset*]
 #     This parameter indicates offset on JBoss ports, defined by socket binding group. It will shift ports up
 #     or down by amount described. The format is simply `n` or `-n`, for example: `120` will shift all ports by
 #     120 up making standard http port being now `8080 + 120 = 8200`. By default it is equal to `0`.
 # [*system_properties*]
-#     This parameter can be used to set system properties that will be passed to server instance java 
+#     This parameter can be used to set system properties that will be passed to server instance java
 #     process as `-D` parameters. It's empty by default.
 # [*jvmopts*]
 #     This property can be set to configure additional JVM options to be passed to server instances in
@@ -102,12 +102,8 @@ define jboss::domain::servergroup (
   }
 
   if $ensure == 'present' {
-    JBoss::Clientry["jboss::domain::servergroup(${name})"] ->
-    JBoss::Clientry["jboss::domain::servergroup::jvm(${name})"]
+    JBoss::Clientry["jboss::domain::servergroup(${name})"] -> JBoss::Clientry["jboss::domain::servergroup::jvm(${name})"]
   } else {
-    JBoss::Clientry["jboss::domain::servergroup::jvm(${name})"] ->
-    JBoss::Clientry["jboss::domain::servergroup(${name})"]
+    JBoss::Clientry["jboss::domain::servergroup::jvm(${name})"] -> JBoss::Clientry["jboss::domain::servergroup(${name})"]
   }
 }
-
-
