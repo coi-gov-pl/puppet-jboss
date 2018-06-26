a85b2c9a2b85f94877cfdf42098db9602f62c8dd
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2018-06-26 18:12:31 +0200

    Fixing acceptance test for Ubuntu 14.04

--- examples/version/as7/cleanup.pp
+++ examples/version/as7/cleanup.pp
@@ -1,6 +1,8 @@
 # JBoss AS 7 do not work on Java >= 7
 if $::osfamily == 'RedHat' {
   $java_package = 'java-1.6.0-openjdk'
+} elsif $::osfamily == 'Debian' {
+  $java_package = 'openjdk-6-jre-headless'
 } else {
   $java_package = undef
 }
