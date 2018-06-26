a9c12a0b1d57679d5f0ed5c0693e192633d1ab9d
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2018-06-26 13:29:26 +0200

    Unit tests are fixed

--- manifests/internal/configure/interfaces.pp
+++ manifests/internal/configure/interfaces.pp
@@ -11,19 +11,17 @@ class jboss::internal::configure::interfaces {
   $bind_public = $jboss::params::bind_public
   $bind_mgmt = $jboss::params::bind_mgmt
 
-  Jboss::Interface {
-    ensure  => 'present',
-  }
-
   $__console_is_sensible = $jboss::internal::runtime::dc::runs_as_controller or $jboss::runasdomain == false
   if $__console_is_sensible and ($enableconsole or $bind_mgmt != undef){
     if $bind_mgmt != undef {
       jboss::interface { 'management':
+        ensure       => 'present',
         inet_address => $bind_mgmt,
         any_address  => undef,
       }
     } else {
       jboss::interface { 'management':
+        ensure       => 'present',
         any_address  => true,
         inet_address => undef,
       }
@@ -32,11 +30,13 @@ class jboss::internal::configure::interfaces {
 
   if $bind_public != undef {
     jboss::interface { 'public':
+      ensure       => 'present',
       any_address  => undef,
       inet_address => $bind_public,
     }
   } else {
     jboss::interface { 'public':
+      ensure       => 'present',
       any_address  => true,
       inet_address => undef,
     }
--- spec/functions/jboss_hash_setvalue_spec.rb
+++ spec/functions/jboss_hash_setvalue_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper_puppet'
 describe 'jboss_hash_setvalue', :type => :puppet_function do
   describe 'given invalid number of arguments(4)' do
     it do
-      should run.
+      is_expected.to run.
         with_params(1, 2, 1, 1).and_raise_error(
           Puppet::ParseError,
           'jboss_hash_setvalue(): wrong lenght of input given (4 for 3)'
@@ -13,7 +13,7 @@ describe 'jboss_hash_setvalue', :type => :puppet_function do
 
   describe 'given invalid number of arguments(2)' do
     it do
-      should run.
+      is_expected.to run.
         with_params(1, 2).and_raise_error(
           Puppet::ParseError,
           'jboss_hash_setvalue(): wrong lenght of input given (2 for 3)'
@@ -22,17 +22,16 @@ describe 'jboss_hash_setvalue', :type => :puppet_function do
   end
 
   describe "given input => { 'john' = 'cena' }, 'adam', 'smith' it should return => { john => 'cena', adam => 'smith' }" do
-    before { skip('FIXME: Method should return hash not only last key, ref coi-gov-pl/puppet-jboss#28 ') }
+    before(:each) do
+      skip('FIXME: Method should return hash not only last key, ref coi-gov-pl/puppet-jboss#28 ')
+    end
     let(:input) { { 'john' => 'cena' } }
     it do
-      should run.
-        with_params(input, 'adam', 'smith').and_return('smith')
-    end
-
-    after(:each) do
-      expect(input).to include('adam' => 'smith')
-      expect(input).to include('john' => 'cena')
-      expect(input.size).to eq(2)
+      is_expected.to run.
+        with_params(input, 'adam', 'smith').and_return(
+          'john' => 'cena',
+          'adam' => 'smith'
+        )
     end
   end
 end
